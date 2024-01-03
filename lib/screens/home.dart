76cece693ff20edacb9616437c9a708c7a648c98
ttnppedr <priest0204@gmail.com>
2024-01-03 18:29:22 +0800

    Add add new todo component

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -42,7 +42,66 @@ class Home extends StatelessWidget {
                 ),
               ],
             ),
-          )
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(
+                      bottom: 20,
+                      right: 20,
+                      left: 20,
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
+                    ),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.grey,
+                            offset: Offset(0, 0),
+                            blurRadius: 10.0,
+                            spreadRadius: 0.0),
+                      ],
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Add a new todo item',
+                        border: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                    bottom: 20,
+                    right: 20,
+                  ),
+                  child: ElevatedButton(
+                      child: Text(
+                        '+',
+                        style: TextStyle(
+                          fontSize: 40,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {
+                        print('Click on Add button');
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: tdBlue,
+                        minimumSize: Size(60, 60),
+                        elevation: 10,
+                      )),
+                )
+              ],
+            ),
+          ),
         ],
       ),
     );
